63a563408c3ae1a05e6c35a5cdec946dc4511c75
cecil john tantay <cjbtantay@gmail.com>
2019-05-13 13:03:26 -0700

    adds horse feature

--- client/src/components/TestContainer/TestContainer.js
+++ client/src/components/TestContainer/TestContainer.js
@@ -2,7 +2,14 @@ import React from "react";
 import { connect } from "react-redux";
 
 const TestContainer = props => {
-  return <img src={props.greeting} alt="It's working gif" />;
+  return (
+    <div>
+      <div className="horse-container">
+        <div className="horse">🐴🐎</div>
+      </div>
+      <img src={props.greeting} alt="It's working gif" />
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
